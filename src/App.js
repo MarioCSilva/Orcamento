3bac6365d292bfe5a3a398dfc37f1203b76cf2f8
Mário Silva <mariocsilva119@gmail.com>
2022-08-03 23:44:51 +0100

    removed pdf feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { createContext, useEffect, useState, createRef } from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { createContext, useEffect, useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import { Pie, Bar } from "react-chartjs-2";
 import { DarkModeSwitch } from 'react-toggle-dark-mode';
 import BasicCard from './Card.js';
@@ -9,64 +9,61 @@ import InputNumber from 'react-input-number';
 import IconButton from "@material-ui/core/IconButton";
 import Tooltip from "@material-ui/core/Tooltip";
 import { Icon } from "@material-ui/core"
-import Pdf from "react-to-pdf";
 import "chartjs-plugin-datalabels";
 import 'chart.js/auto';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const ThemeContext = createContext(null);
-const ref = createRef();
 
 
+function App() {
+  const [dataVals, setDataVals] = useState(null);
+  const [capitalVals, setCapitalVals] = useState(null)
+  const [wordMonths, setWordMonths] = useState("Meses");
+  const [wordYears, setWordYears] = useState("Anos");
+  const years = valuesStore(state => state.years);
+  const months = valuesStore(state => state.months);
+  const data = valuesStore(state => state.data);
+  const totalSpent = valuesStore(state => state.totalSpent);
+  const theme = valuesStore(state => state.theme);
+  const [isDarkMode, setDarkMode] = useState(theme === "dark" ? true : false);
 
-var getTotal = function (dataVals, value) {
-  var sum = dataVals.datasets[0].data.reduce(
-    (a, b) => a + b,
-    0
+  const getTotal = (dataVals, value) => {
+    var sum = dataVals.datasets[0].data.reduce(
+      (a, b) => a + b,
+      0
     );
     if ((value / sum * 100).toFixed(0) !== '0')
-    return (value / sum * 100).toFixed(0) + "%";
+      return (value / sum * 100).toFixed(0) + "%";
     return null
   };
-  
-  function App() {
-    const [dataVals, setDataVals] = useState(null);
-    const [capitalVals, setCapitalVals] = useState(null)
-    const [wordMonths, setWordMonths] = useState("Meses");
-    const [wordYears, setWordYears] = useState("Anos");
-    const years = valuesStore(state => state.years);
-    const months = valuesStore(state => state.months);
-    const data = valuesStore(state => state.data);
-    const totalSpent = valuesStore(state => state.totalSpent);
-    const theme = valuesStore(state => state.theme);
-    const [isDarkMode, setDarkMode] = useState(theme === "dark" ? true : false);
-    
-    const InfoIcon = () => (
-      <Icon>
-          <img alt={''} src={theme !== "light" ? require('./info-light.png') : require('./info.png')} style={{width: 20, height: 20}} id={theme}/>
-      </Icon>
-    )
-    
-    const DoneIcon = () => (
-      <Icon>
-          <img alt={''} src={theme !== "light" ? require('./done-light.png') : require('./done.png')} style={{width: 20, height: 20}}/>
-      </Icon>
-    )
-    
-    const ClearIcon = () => (
-      <Icon>
-          <img alt={''} src={theme !== "light" ? require('./clear-light.png') : require('./clear.png')} style={{width: 20, height: 20}}/>
-      </Icon>
-    )
 
-    useEffect(()=>{
+  const InfoIcon = () => (
+    <Icon>
+      <img alt={''} src={theme !== "light" ? require('./info-light.png') : require('./info.png')} style={{ width: 20, height: 20 }} id={theme} />
+    </Icon>
+  )
+
+  const DoneIcon = () => (
+    <Icon>
+      <img alt={''} src={theme !== "light" ? require('./done-light.png') : require('./done.png')} style={{ width: 20, height: 20 }} />
+    </Icon>
+  )
+
+  const ClearIcon = () => (
+    <Icon>
+      <img alt={''} src={theme !== "light" ? require('./clear-light.png') : require('./clear.png')} style={{ width: 20, height: 20 }} />
+    </Icon>
+  )
+
+  useEffect(() => {
     if (parseFloat(years) === parseFloat(1))
       setWordYears("Ano")
     else
       setWordYears("Anos")
 
-  if (parseFloat(months) === parseFloat(1))
+    if (parseFloat(months) === parseFloat(1))
       setWordMonths("Mês")
     else
       setWordMonths("Meses")
@@ -118,15 +115,15 @@ var getTotal = function (dataVals, value) {
   const barChartOptions = {
     responsive: true,
     scales: {
-      yAxes:{
-          ticks:{
-              color: theme === 'light' ? '#23272a' : 'white'
-          }
+      yAxes: {
+        ticks: {
+          color: theme === 'light' ? '#23272a' : 'white'
+        }
       },
       xAxes: {
-          ticks:{
-              color: theme === 'light' ? '#23272a' : 'white'
-          }
+        ticks: {
+          color: theme === 'light' ? '#23272a' : 'white'
+        }
       },
     },
     plugins: {
@@ -200,50 +197,20 @@ var getTotal = function (dataVals, value) {
     setDataVals(newDataVals);
   }, [data, theme])
 
-  const [options, setOptions] = useState({
-    unit: 'in',
-    format: [
-      (ref.current ? ref.current.offsetWidth : window.innerWidth) / 96,
-      (ref.current ? ref.current.offsetHeight : window.innerHeight) / 96
-    ]
-  });
-
-  const handleResize = () => {
-    setOptions({
-      unit: 'in',
-      format: [
-        (ref.current ? ref.current.offsetWidth : window.innerWidth) / 96,
-        (ref.current ? ref.current.offsetHeight : window.innerHeight) / 96
-      ]
-    })
-  }
-
-  useEffect(() => {
-    console.log(options.format)
-  }, [options])
-
-  useEffect(() => {
-    window.addEventListener("resize", handleResize, false);
-  }, []);
-
-
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className="App" style={{ width: '100%', overflowX: 'hidden' }} id={theme}>
         <header className="App-header">
-          <Container ref={ref} id={theme} style={{paddingTop: 20}}>
+          <Container id={theme} style={{ paddingTop: 20 }}>
             <Row>
               <Col xs="3" sm="4">
-                <Pdf targetRef={ref} filename="orcamento.pdf" options={options} scale={1}>
-                  {({ toPdf }) => <Button variant={theme} onClick={toPdf} style={{fontWeight: 600, fontSize: '.75rem', float: 'left'}}>PDF</Button>}
-                </Pdf>
               </Col>
               <Col xs="6" sm="4">
-                <h2 id={theme} style={{ fontWeight: '900', width: '100%'}}>ORÇAMENTO</h2>
+                <h2 id={theme} style={{ fontWeight: '900', width: '100%' }}>ORÇAMENTO</h2>
               </Col>
               <Col xs="3" sm="4">
                 <DarkModeSwitch
-                  style={{float: 'right'}}
+                  style={{ float: 'right' }}
                   checked={isDarkMode}
                   onChange={toggleTheme}
                   size={36}
@@ -256,22 +223,23 @@ var getTotal = function (dataVals, value) {
               </Col>
               <Col xs="12" sm="12" md="6" lg="4" style={{ paddingTop: 20 }}>
                 <BasicCard id="Património" cardTitle="Património" cardTable={["Investimentos", "Poupança", "Reserva de emergência"]} />
-                <div style={{borderRadius: '5px', marginTop: '20px', height: 95, 
+                <div style={{
+                  borderRadius: '5px', marginTop: '20px', height: 95,
                   boxShadow: theme === 'light' ?
-                  '0 0px 10px rgba(108, 117, 125,.08), 0 0 6px rgba(108, 117, 125,.05)' :
-                  '0 0px 10px rgba(248, 249, 250,.08), 0 0 6px rgba(248, 249, 250,.05)',
+                    '0 0px 10px rgba(108, 117, 125,.08), 0 0 6px rgba(108, 117, 125,.05)' :
+                    '0 0px 10px rgba(248, 249, 250,.08), 0 0 6px rgba(248, 249, 250,.05)',
                   border: theme !== 'light' ? '#f8f9fa' : '#212529',
                   background: theme === 'light' ? '#f8f9fa' : '#6c757d'
                 }}>
-                  <h6 style={{ fontWeight: 500, paddingTop: 24, background: 'transparent', height: '100%',  color: theme !== 'light' ? 'white' : '#23272a'}}>
+                  <h6 style={{ fontWeight: 500, paddingTop: 24, background: 'transparent', height: '100%', color: theme !== 'light' ? 'white' : '#23272a' }}>
                     Orçamento Zero
                     {
                       data['Rendimentos']['total'].toFixed(2) - totalSpent.toFixed(2) === 0 ?
-                        <DoneIcon style={{color: theme === 'light' ? '#141619' : 'white', paddingBottom: 5}}/> :
-                        <ClearIcon style={{color: theme !== 'light' ? 'white' : '#141619', paddingBottom: 5}}/>
+                        <DoneIcon style={{ color: theme === 'light' ? '#141619' : 'white', paddingBottom: 5 }} /> :
+                        <ClearIcon style={{ color: theme !== 'light' ? 'white' : '#141619', paddingBottom: 5 }} />
                     }
-                    <br/>
-                    {data['Rendimentos']['total'].toFixed(2)} € - {totalSpent.toFixed(2)} € = <span style={{paddingLeft: 3, paddingRight: 3, borderRadius: '5%', fontWeight: '900' }}>{(data['Rendimentos']['total'] - totalSpent).toFixed(2)} €</span>
+                    <br />
+                    {data['Rendimentos']['total'].toFixed(2)} € - {totalSpent.toFixed(2)} € = <span style={{ paddingLeft: 3, paddingRight: 3, borderRadius: '5%', fontWeight: '900' }}>{(data['Rendimentos']['total'] - totalSpent).toFixed(2)} €</span>
                   </h6>
                 </div>
               </Col>
@@ -297,10 +265,10 @@ var getTotal = function (dataVals, value) {
                 <BasicCard id="Extras" cardTitle="Extras" cardTable={["Dívidas (cartões de crédito)", "Crédito Pessoal", "Outros créditos", "Restaurantes / bares / cafés", "Subscrições", "Livros  e workshops", "Reparações / arranjos", "Utensílios", "Férias", "Multas"]} />
               </Col>
             </Row>
-            <div id={theme} style={{width: '100%', paddingTop: 30}}>
-              <h4 style={{fontWeight: '900'}}>
+            <div id={theme} style={{ width: '100%', paddingTop: 30 }}>
+              <h4 style={{ fontWeight: '900' }}>
                 Capital após <InputNumber
-                  style={{ fontWeight: 900, width: (months.toFixed(0).length)*20, textAlign: 'center', border: 'none', background: 'transparent', color: theme === 'light' ? '#23272a': 'white'}}
+                  style={{ fontWeight: 900, width: (months.toFixed(0).length) * 20, textAlign: 'center', border: 'none', background: 'transparent', color: theme === 'light' ? '#23272a' : 'white' }}
                   min={0}
                   value={months}
                   max={100000}
@@ -308,7 +276,7 @@ var getTotal = function (dataVals, value) {
                   onChange={(value) => changeMonths(value)}
                   enableMobileNumericKeyboard
                 /> {wordMonths} e <InputNumber
-                  style={{ fontWeight: 900, width: (years.toFixed(0).length)*20, textAlign: 'center', border: 'none', background: 'transparent', color: theme === 'light' ? '#23272a': 'white'}}
+                  style={{ fontWeight: 900, width: (years.toFixed(0).length) * 20, textAlign: 'center', border: 'none', background: 'transparent', color: theme === 'light' ? '#23272a' : 'white' }}
                   min={0}
                   value={years}
                   max={99}
@@ -317,13 +285,13 @@ var getTotal = function (dataVals, value) {
                   enableMobileNumericKeyboard
                 /> {wordYears}
                 <Tooltip title="Edita o número de meses e anos!">
-                <IconButton style={{marginBottom: 9, cursor: 'default', marginLeft: 5}}>
-                  <InfoIcon style={{color: theme === 'light' ? '#23272a': 'white'}}/>
-                </IconButton>
-              </Tooltip>
+                  <IconButton style={{ marginBottom: 9, cursor: 'default', marginLeft: 5 }}>
+                    <InfoIcon style={{ color: theme === 'light' ? '#23272a' : 'white' }} />
+                  </IconButton>
+                </Tooltip>
               </h4>
               <Row>
-                { dataVals &&
+                {dataVals &&
                   <>
                     <Bar plugins={[ChartDataLabels]} options={barChartOptions} data={capitalVals} style={{ maxHeight: 356.5, textAlign: "center", paddingTop: 15 }} />
                   </>
